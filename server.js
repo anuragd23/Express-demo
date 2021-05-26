2e9eb9559a5f9a09898b50d70b8066d807792693
Anurag Das <anurag.das23.ad@gmail.com>
2021-05-26 19:48:58 -0400

    setting up friends and messages routers

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const express = require('express');
 
-const friendsController = require('./controllers/friends.controller');
+const friendsRouter = require('./routes/friends.router');
 
-const messagesController = require('./controllers/messages.controller');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
 const PORT = 3000;
 
-
 app.use((req, res, next) => {
     const start = Date.now();
     next();
     const delta = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${delta}ms`);
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post('/friends', friendsController.postFriend);
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId', friendsController.getFriend);
+app.use('/friends', friendsRouter);
 
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
+app.use('/messages', messagesRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`)
