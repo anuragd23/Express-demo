c24d27e1c0f42f4102abc3808530ed831776a338
Anurag Das <anurag.das23.ad@gmail.com>
2021-05-26 20:15:00 -0400

    Sending files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hello Friend!</li></ul>');
+  //  res.send('<ul><li>Hello Friend!</li></ul>');
+  res.sendFile( path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+
 }
 
 function postMessage(req, res) {
